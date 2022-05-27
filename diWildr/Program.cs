16f5ef3cd4ace700aebd125076c0076b4603abae
Ximena Valenzuela <xduarte@uabc.edu.mx>
2022-05-27 01:45:06 -0700

    Agregar archivos de proyecto.

---
+++ diWildr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diWildr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 15;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+                Console.ReadKey();
+            }
+            while (i <= 10);
+
+
+        }
+
+    }
